ab667756fd929afd67e1cb55cb959695726cab93
Taly <vitalik7tv@yandex.ru>
2019-03-31 17:20:21 +0300

    Add comments, readme and license

---
+++ README.md
@@ -0,0 +1,42 @@
+# Radio Record tray player
+
+This is a test Electron tray app with a Radio Record [embed player](http://www.radiorecord.ru/player/).
+
+Radio Record — https://www.radiorecord.ru
+
+![](https://capella.pics/d9700ee9-2df3-49ce-a9d3-40de1327ff68.jpg)
+
+## Download
+
+Visit [releases page](https://github.com/talyguryn/record/releases) to get the latest version.
+
+## Development
+
+Clone the repository
+
+```
+git clone https://github.com/talyguryn/record
+cd record
+```
+
+Install node dependencies
+
+```
+yarn
+```
+
+Run the app
+
+```
+yarn start
+```
+
+Build the release
+
+```
+yarn build
+```
+
+## Contribution
+
+Feel free to create issues, fork repository and improve any part of this code.
--- index.js
+++ index.js
@@ -1,41 +1,94 @@
+/**
+ * Embed player URL
+ * @type {string}
+ */
 const RECORD_URL = 'https://radiorecord.ru/player/';
 
+/**
+ * Require tools
+ */
 const { app, BrowserWindow, Tray, Menu } = require('electron');
 const path = require('path');
 
-let tray = undefined;
-let window = undefined;
+/**
+ * Define global vars
+ */
+let tray;
+let window;
 
+/**
+ *
+ */
 const createTray = () => {
+  /**
+   * Add app to tray
+   */
   tray = new Tray(path.join(__dirname, 'assets', 'tray-icon-Template.png'));
+
+  /**
+   * Listen click event
+   */
   tray.on('click', function (event) {
     toggleWindow();
   });
 };
 
+/**
+ * Toggle window helper
+ */
 const toggleWindow = () => {
-  window.isVisible() ? window.hide() : showWindow();
-};
+  /**
+   * Toggle window
+   */
+  if (window.isVisible()) {
+    /**
+     * Hide window
+     */
+    window.hide()
+  } else {
+    /**
+     * Recalculate window position
+     */
+    const position = getWindowPosition();
 
-const showWindow = () => {
-  const position = getWindowPosition();
-  window.setPosition(position.x, position.y, false);
-  window.show();
-};
+    /**
+     * Move window
+     */
+    window.setPosition(position.x, position.y, false);
 
+    /**
+     * Show window
+     */
+    window.show();
+  }
+}
+
+/**
+ * Calculate window position
+ */
 const getWindowPosition = () => {
-  const windowBounds = window.getBounds();
-  const trayBounds = tray.getBounds();
+  /**
+   * Get window and tray bounds
+   */
+  const windowBounds = window.getBounds(),
+        trayBounds = tray.getBounds();
 
-  // Center window horizontally below the tray icon
+  /**
+   * Center window horizontally below the tray icon
+   */
   const x = Math.round(trayBounds.x + (trayBounds.width / 2) - (windowBounds.width / 2));
 
-  // Position window 4 pixels vertically below the tray icon
+  /**
+   * Position window 4 pixels vertically below the tray icon
+   */
   const y = Math.round(trayBounds.y + trayBounds.height + 4);
 
-  return {x: x, y: y}
+  return {x, y}
 };
 
+/**
+ * Create a new window
+ */
 const createWindow = () => {
   window = new BrowserWindow({
     width: 680,
@@ -46,13 +99,22 @@ const createWindow = () => {
     resizable: false,
     transparent: true,
     webPreferences: {
+      /**
+       * Disable Node.js integration of Electron
+       * https://electronjs.org/docs/faq#i-can-not-use-jqueryrequirejsmeteorangularjs-in-electron
+       */
       nodeIntegration: false
     }
   });
 
+  /**
+   * Load target webpage
+   */
   window.loadURL(RECORD_URL);
 
-  // Hide the window when it loses focus
+  /**
+   * Hide the window when it loses focus
+   */
   window.on('blur', () => {
     if (!window.webContents.isDevToolsOpened()) {
       window.hide();
@@ -60,12 +122,24 @@ const createWindow = () => {
   });
 };
 
-// Don't show the app in the doc
+/**
+ * Don't show app in the dock
+ */
 app.dock.hide();
 
+/**
+ * On ready initial function
+ */
 app.on('ready', () => {
   try {
+    /**
+     * Prepare tray icon
+     */
     createTray();
+
+    /**
+     * Prepare window
+     */
     createWindow();
   } catch (error) {
     console.log(error);
